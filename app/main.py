3b45d99e68605ce00e3cae97b64e49704fba7452
suhwan87 <tnghks4875@naver.com>
2025-04-07 10:46:19 +0900

    fin_3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api import router
+import warnings
 
 app = FastAPI()
 app.include_router(router)
@@ -7,3 +8,6 @@ app.include_router(router)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    
+
+warnings.filterwarnings("ignore")
