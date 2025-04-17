2908178ebe5eafaecf0e0c537b91c224e820e813
suhwan87 <tnghks4875@naver.com>
2025-04-17 17:33:26 +0900

    망했다

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=10000, reload=True)
     
 warnings.filterwarnings("ignore")
 
